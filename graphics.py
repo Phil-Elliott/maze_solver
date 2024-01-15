64fd05a56b592ca3af9e8cf7c542534abe93fe3f
Phil <phillippelliott@gmail.com>
2024-01-15 11:51:57 +0800

    finished project

--- graphics.py
+++ graphics.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, BOTH, Canvas
 
+
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
@@ -22,6 +23,27 @@ class Window:
     def draw_line(self, line, fill_color="black"):
         line.draw(self.__canvas, fill_color)
 
-
     def close(self):
         self.__running = False
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(
+        self,
+        p1,
+        p2,
+    ):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
+        canvas.pack(fill=BOTH, expand=1)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from window import Window
+from graphics import Window
 from maze import Maze
+import sys
+
 
 def main():
     num_rows = 12
@@ -9,37 +11,18 @@ def main():
     screen_y = 600
     cell_size_x = (screen_x - 2 * margin) / num_cols
     cell_size_y = (screen_y - 2 * margin) / num_rows
+
+    sys.setrecursionlimit(10000)
     win = Window(screen_x, screen_y)
 
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
-
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
+    print("maze created")
+    is_solveable = maze.solve()
+    if not is_solveable:
+        print("maze can not be solved!")
+    else:
+        print("maze solved!")
     win.wait_for_close()
 
 
 main()
-
-# def main():
-#     win = Window(800, 600)
-
-#     c1 = Cell(win)
-#     c1.has_left_wall = False
-#     c1.draw(50, 50, 100, 100)
-
-#     c2 = Cell(win)
-#     c2.has_right_wall = False
-#     c2.draw(125, 125, 200, 200)
-#     c2.draw_move(c1)
-
-#     c3 = Cell(win)
-#     c3.has_bottom_wall = False
-#     c3.draw(225, 225, 250, 250)
-#     c3.draw_move(c2)
-
-#     c4 = Cell(win)
-#     c4.has_top_wall = False
-#     c4.draw(300, 300, 500, 500)
-#     c4.draw_move(c3)
-
-#     win.wait_for_close()
-
-# main()
